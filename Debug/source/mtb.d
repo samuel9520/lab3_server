3aeae036dec15d45021ce3a984f67c50797acc02
Adam Green <ajsg1g15@soton.ac.uk>
2017-12-08 14:08:29 +0000

    Fixed gitignore issues

--- Debug/source/mtb.d
+++
@@ -1,4 +0,0 @@
-source/mtb.o source/mtb.d: ../source/mtb.c \
- /home/samuel9520/Documents/MCUXpresso_10.0.2_411/workspace/lab3_server/source/app_preinclude.h
-
-/home/samuel9520/Documents/MCUXpresso_10.0.2_411/workspace/lab3_server/source/app_preinclude.h:
